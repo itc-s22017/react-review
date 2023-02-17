b62b17ce91ded655d5a2b398b80aabf99bd8d438
itc-s22017 <s22017@std.it-college.ac.jp>
2023-02-17 06:20:40 +0000

    9.5

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,10 @@
 import Hero from 'components/hero'
 import Container from 'components/container'
 import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <>
       <Container>
@@ -11,9 +13,20 @@ const Blog = () => {
           title='BLOG'
           subtitle='RECENT POST'
         />
+
+        <Posts posts={posts} />
       </Container>
     </>
   )
 }
 
+export const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 export default Blog
